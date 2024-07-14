9bb4c2e6c9e72895b4fd6f177405bbe2b5da57e1
pachipalasiva <120707070+pachipalasiva@users.noreply.github.com>
2024-07-14 11:41:20 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,68 +5,50 @@
 int parse_input(char *input_string, char *delimiter) {
     if (strncmp(input_string, "//", 2) == 0) {
         *delimiter = input_string[2];
-        return 4;
+        return atoi(input_string + 4);
     }
     *delimiter = ',';
-    return 0;
+    return atoi(input_string);
 }
 
-int *split_and_convert(char *input_string, char delimiter, int *count) {
+int split_and_convert(char *input_string, char delimiter, int *numbers, int *count) {
     char *token;
-    char *delim = malloc(2 * sizeof(char));
+    char *delim = malloc(2);
     delim[0] = delimiter;
     delim[1] = '\0';
-    int *numbers = malloc(0 * sizeof(int));
     *count = 0;
-
     token = strtok(input_string, delim);
     while (token != NULL) {
-        numbers = realloc(numbers, (*count + 1) * sizeof(int));
         numbers[*count] = atoi(token);
         (*count)++;
         token = strtok(NULL, delim);
     }
-
     free(delim);
-    return numbers;
+    return *count;
 }
 
 int filter_numbers(int *numbers, int count) {
-    int filtered_count = 0;
-    int *filtered_numbers = malloc(0 * sizeof(int));
-
-    for (int i = 0; i < count; i++) {
+    int i, j = 0;
+    for (i = 0; i < count; i++) {
         if (numbers[i] <= 1000) {
-            filtered_numbers = realloc(filtered_numbers, (filtered_count + 1) * sizeof(int));
-            filtered_numbers[filtered_count] = numbers[i];
-            filtered_count++;
+            numbers[j++] = numbers[i];
         }
     }
-
-    free(numbers);
-    return filtered_count;
+    return j;
 }
 
 int add(char *input_string) {
-    if (input_string[0] == '\0') {
+    if (strlen(input_string) == 0) {
         return 0;
     }
-
     char delimiter;
-    int offset = parse_input(input_string, &delimiter);
-    char *parsed_input = input_string + offset;
-
+    int parsed_input = parse_input(input_string, &delimiter);
+    int numbers[1000];
     int count;
-    int *numbers = split_and_convert(parsed_input, delimiter, &count);
-
-    int filtered_count = filter_numbers(numbers, count);
+    int filtered_count = split_and_convert(input_string + parsed_input, delimiter, numbers, &count);
     int sum = 0;
     for (int i = 0; i < filtered_count; i++) {
         sum += numbers[i];
     }
-
-    free(numbers);
     return sum;
 }
-
-
